1abe5851f266ae4a2c5c9d90e23233168c8b2b29
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-30 14:32:20 +0200

    Define model method that update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    author.update(post_counter: author.posts.count)
+  end
 end
