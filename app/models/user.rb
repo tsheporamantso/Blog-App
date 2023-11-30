528a4fc5e961ee1fe9205c6e1a4e6fd8e0b04a05
tsheporamantso <tshepo.ramantso@outlook.com>
2023-11-30 14:31:36 +0200

    Correct indentation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :posts, foreign_key: :author_id
-end
 
-def three_recent_posts
-  posts.order(created_at: :desc).limit(3)
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
