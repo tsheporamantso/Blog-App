666f7760a7e3931b0b0e9779984538208eb961a1
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-01 15:14:03 +0200

    Update rails_helper.rb to require factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'factory_bot_rails'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -62,4 +63,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  RSpec.configure do |rspec_config|
+    rspec_config.include FactoryBot::Syntax::Methods
+  end
 end
