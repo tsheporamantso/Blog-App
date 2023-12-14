efc45f2ac32259b70f5d644d17fa211e6eda86a5
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-14 16:17:06 +0200

    Add undefined methods set_user_and_post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,11 @@ class CommentsController < ApplicationController
   before_action :set_user_and_post, only: %i[new create]
 
   def new
+    store_referer
+    @comment = Comment.new
+  end
+
+  def create
     @comment = @post.comments.build(comment_params.merge(user_id: current_user.id))
 
     if @comment.save
@@ -17,6 +22,11 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text)
+  end
+
+  def set_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
   end
 
   def store_referer
