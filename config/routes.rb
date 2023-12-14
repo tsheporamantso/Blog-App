978ed0ebe7040eacc4baac32ad800f8975882824
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-14 12:27:19 +0200

    Add resources routing for comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
   root "users#index"
-  resources :users, only:[:index, :show] do
-    resources :posts, only:[:index, :show, :new, :create] do
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
     end
   end
 
