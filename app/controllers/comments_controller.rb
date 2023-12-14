661bb3babecd55a2dda24cc92db0ce0cc05e1947
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-14 12:21:57 +0200

    Define new and create methods that creates a comment on behalf of current_user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+  before_action : set_user_and_post, only: %i[new create]
+
+  def new
+    @comment = @post.comments.build(comment_params.merge(user_id: current_user.id))
+
+    if @comment.save
+        flash[:success] = 'Comment saved successfully'
+        redirect_back_or_default(user_post_comments_path(@user, @post))
+    else
+        flash.now[:error] = 'Error: Comment could not be saved'
+        render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def store_referer
+    session[:referer] = request.referer
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:referer] || default)
+    session.delete(:referer)
+  end
 end
