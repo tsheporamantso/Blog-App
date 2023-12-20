e9c1fc8151196eda0cf2d994baf9cf3b8570422d
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-20 09:45:37 +0200

    Add spec test case to display correct number user and display all user photos

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,16 +1,22 @@
-# raquire 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'User index', type: :feature do
-#     let(:users) { FactoryBot.create_list(user, 5) }
+RSpec.describe 'User index', type: :feature do
+  let(:users) { FactoryBot.create_list(:user, 4) }
 
-#     before do
-#       users
-#       visit users_path
-#     end
+  before do
+    users
+    visit users_path
+  end
+  
+  describe 'User index page' do
+    it 'displays correct number of users' do
+      expect(page).to have_css('div.user_info', count: 4)
+    end
 
-#     describe 'User index page' do
-#       it 'displays correct number of users' do
-#         expect(page).to have css('div.user_info', count: 5)
-#       end
-#     end
-# end
+    it 'displays all users photo' do
+      users.each do |_user|
+        expect(page).to have_css("img[alt='User Photo']")
+      end
+    end
+  end 
+end
