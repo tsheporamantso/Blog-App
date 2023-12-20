b833b2acd054b053a894318ed1ef49416f5cc1c8
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-20 12:23:57 +0200

    Add post index spec to show username and user photo

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,4 +1,4 @@
-require 'rails helper'
+require 'rails_helper'
 
 RSpec.describe 'PostIndex', type: :feature do
   let(:user) { FactoryBot.create(:user) }
@@ -6,4 +6,23 @@ RSpec.describe 'PostIndex', type: :feature do
   let(:comments) { FactoryBot.create_list(:comment, 3, post:, text: 'old', author: user, created_at: 4.days.ago) }
 
   let(:recent_comments) { FactoryBot.create_list(:comment, 5, post:, text: 'new', author: user) }
+
+  before do
+    post
+    comments
+    recent_comments
+    visit user_posts_path(user)
+  end
+
+  describe 'User section in user post index' do
+    it 'shows user name' do
+      expect(page).to have_content(user.name)
+    end
+
+    it 'shows user photo' do
+      expect(page).to have_css("img[alt= 'User Photo']")
+    end
+
+    
+  end
 end
