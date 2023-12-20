9e71cfdcec4166042bee16762cb04fa239fdb635
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-20 10:25:16 +0200

    Correct typos

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User index', type: :feature do
-  let(:users) { FactoryBot.create_list(:user, 4) }
+  let(:users) { FactoryBot.create_list(:user, 5) }
 
   before do
     users
@@ -10,7 +10,7 @@ RSpec.describe 'User index', type: :feature do
   
   describe 'User index page' do
     it 'displays correct number of users' do
-      expect(page).to have_css('div.user_info', count: 4)
+      expect(page).to have_css('div.user_info', count: 5)
     end
 
     it 'displays all users photo' do
@@ -18,20 +18,22 @@ RSpec.describe 'User index', type: :feature do
         expect(page).to have_css("img[alt='User Photo']")
       end
     end
+
     it 'displays all users names' do
       users.each do |user|
         expect(page).to have_content(user.name)
       end
     end
+
     it 'displays all users posts count' do
       users.each do |user|
         expect(page).to have_content(user.posts_counter)
       end
     end
+
     it 'redirects to user show page' do
       find_all('a', text: users.first.name)[0].click
       expect(page).to have_current_path(user_path(users.first))
-    end
-  end
+     end
   end 
 end
