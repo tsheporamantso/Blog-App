964e18d1cc5ca483f00f5e5c6110ac5d570e7258
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-20 08:30:05 +0200

    Add feature folder in spec folder with user index spec file

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,16 @@
+# raquire 'rails_helper'
+
+# RSpec.describe 'User index', type: :feature do
+#     let(:users) { FactoryBot.create_list(user, 5) }
+
+#     before do
+#       users
+#       visit users_path
+#     end
+
+#     describe 'User index page' do
+#       it 'displays correct number of users' do
+#         expect(page).to have css('div.user_info', count: 5)
+#       end
+#     end
+# end
