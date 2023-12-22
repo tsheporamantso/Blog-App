2eefd2ba7bde9fc18ccd112ca6e7c49e3ae1c497
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-22 12:23:53 +0200

    Add post index test cases and run Rspec test with Capybara

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -14,15 +14,58 @@ RSpec.describe 'PostIndex', type: :feature do
     visit user_posts_path(user)
   end
 
-  describe 'User section in user post index' do
+  describe 'User section in user posts index' do
     it 'shows user name' do
       expect(page).to have_content(user.name)
     end
 
     it 'shows user photo' do
-      expect(page).to have_css("img[alt= 'User Photo']")
+      expect(page).to have_css("img[alt='User Photo']")
     end
 
-    
+    it 'shows user posts count' do
+      expect(page).to have_content(user.posts_counter)
+    end
+  end
+
+  it 'Shows how many likes post has' do
+    expect(page).to have_content(post.likes_counter)
+  end
+
+  describe 'Posts section in user posts index' do
+    it 'Shows post heading' do
+      expect(page).to have_content('Post')
+    end
+
+    it 'Shows post content' do
+      expect(page).to have_content(post.text)
+    end
+
+    it 'Shows pagination section if there are more posts than fit on the view' do
+      FactoryBot.create_list(:post, 10, author: user)
+      visit user_posts_path(user)
+      expect(page).to have_content('Pagination')
+    end
+
+    it 'Redirects to post show page' do
+      click_link(post.title)
+      expect(page).to have_current_path(user_post_path(user, post))
+    end
+  end
+
+  describe 'Comments section in user posts index' do
+    it 'Shows comment heading' do
+      expect(page).to have_content('Comments:')
+    end
+
+    it 'Shows comments count' do
+      expect(page).to have_content(post.comments_counter)
+    end
+
+    it 'Shows five most recent comments' do
+      recent_comments.reverse.each do |comment|
+        expect(page).to have_content(comment.text)
+      end
+    end
   end
 end
