8405893b83203c990006e78b5667be3e3ace0b47
tsheporamantso <tshepo.ramantso@outlook.com>
2023-12-22 12:05:15 +0200

    Modify author association on factory Bot

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    text { 'Hope you having a wonderful day!' }
+    text { "Hope you're having a wonderful day!" }
     association :author, factory: :user
     association :post, factory: :post
   end
